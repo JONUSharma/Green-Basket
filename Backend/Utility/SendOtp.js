95040213ba36acfb5438e0afba1a3bced8864bfb
Jonu Sharma <jonusharam4440@gmail.com>
2025-12-14 23:24:36 +0530

    update SendOtp from gmail to smtp services

--- Backend/Utility/SendOtp.js
+++ Backend/Utility/SendOtp.js
@@ -5,7 +5,9 @@ const generateOtp = () => Math.floor(100000 + Math.random() * 900000)
 
 const sendOtp = async (email, otp) => {
     const Transporter = nodeMailer.createTransport({
-        service: "gmail",
+        host : "smtp.gmail.com",
+        port : 587,
+        secure : false,
         auth: {
             user: process.env.OTP_EMAIL_USER,
             pass: process.env.OTP_EMAIL_PASS
